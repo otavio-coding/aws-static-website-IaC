17797fd36ccf14e009d6eaccfe6570e34abbeda8
otavio-coding <otavio.a.f.97@gmail.com>
2025-03-14 17:36:15 -0300

    fix(ci): Add aws_iam_openid_connect_provider for OIDC authentication.

--- infra/github_actions.tf
+++ infra/github_actions.tf
@@ -33,3 +33,16 @@ data "aws_iam_policy_document" "s3_deploy_policy" {
     resources = ["arn:aws:s3:::${aws_s3_bucket.website.id}/*"]
   }  
 }
+
+
+
+#Setting up a OIDC provider.
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  client_id_list  = ["sts.amazonaws.com"]
+}
